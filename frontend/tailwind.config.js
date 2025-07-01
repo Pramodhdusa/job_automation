7487c656f6df356476d2ac0d8f00e9f719b86b2f
pramodhdusa <pramodhdusa@gmail.com>
2025-07-01 22:09:25 +0530

    Fix Tailwind CSS and PostCSS config, rebuild project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
-// /** @type {import('tailwindcss').Config} */
+// // frontend/tailwind.config.js
 // module.exports = {
-//   content: [],
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
 //   theme: {
 //     extend: {},
 //   },
 //   plugins: [],
 // }
-
